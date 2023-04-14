cd9f58a0edeaaa952415eceb2754934b5b8b76ab
Je Sian Keith Herman <68434444+jsk-herman@users.noreply.github.com>
2023-04-15 00:39:46 +0800

    Add load test data button

--- About.py
+++ About.py
@@ -1,6 +1,8 @@
+import pandas as pd
+import polars as pl
 import streamlit as st
 
-from load import init_page, load_brand
+from load import init_page, load_brand, load_testdata
 
 init_page(pg_title="About MaMA", pg_icon="🤗", title="About MaMA")
 
@@ -14,7 +16,25 @@ st.markdown(
     or **Ma**lnutrition **M**onitoring and **A**ssessment System is an application
     made by Team NutriBUn for the tracking of various case data of malnutrition
     in the Philippines. This application was originally made in 2022 for the
-    first Nutri-Hackathon of the National Nutrition Council of the Philippines.
+    first Nutri-Hackathon of the National Nutrition Council of the Philippines
+    but now revised and reinvisioned this time around.
     """,
     unsafe_allow_html=True,
 )
+
+# Step 1: Load test data
+
+st.markdown("## Step 1: Load Test Data")
+if st.button("Load Test Data"):
+    st.markdown(
+        """- You can click inside of the table and press
+                <kbd>Ctrl</kbd>+<kbd>F</kbd> in Windows or
+                <kbd>⌘ Cmd</kbd>+<kbd>F</kbd> in Mac to search the data.\n- You
+                can also select cells and copy them to your clipboard with
+                <kbd>Ctrl</kbd>+<kbd>C</kbd> in Windows or
+                <kbd>⌘ Cmd</kbd>+<kbd>C</kbd> in Mac.""",
+        unsafe_allow_html=True,
+    )
+    st.session_state["pl_data"] = load_testdata(30)
+    st.session_state["pd_data"] = st.session_state["pl_data"].to_pandas()
+    st.write(st.session_state["pd_data"])
